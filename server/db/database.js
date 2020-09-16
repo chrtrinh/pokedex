962a23535e070591b89548a628e376f8101e4a75
Christopher Trinh <Chris.trinh@live.com>
2020-09-16 00:56:58 -0400

    made changes to database.js

--- server/db/database.js
+++ server/db/database.js
@@ -15,7 +15,7 @@ const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}_test` : pkg.name;
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
 const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-  logging: false,
+	logging: false,
 });
 
 module.exports = db;
