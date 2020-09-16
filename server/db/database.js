47a1e95499edef565bb4cba106b17fedbb5409f6
Christopher Trinh <Chris.trinh@live.com>
2020-09-16 01:22:19 -0400

    updated database.js

--- server/db/database.js
+++ server/db/database.js
@@ -14,8 +14,11 @@ const pkg = require('../../package.json');
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}_test` : pkg.name;
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-	logging: false,
-});
+const db = new Sequelize(
+	`postgres://tqgbprmxodemwo:b8809c03e33b5303e131998e240c26379c8bfbc5652ba8a24c270f8e80f5e455@ec2-3-95-87-221.compute-1.amazonaws.com:5432/dbn2l8g7t2vqqf/${dbName}`,
+	{
+		logging: false,
+	}
+);
 
 module.exports = db;
